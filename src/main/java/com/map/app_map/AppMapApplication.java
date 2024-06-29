33469d67389eb0e692b151f8f52f2e8b7eb254bd
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2024-06-29 15:34:37 -0500

    init commit

---
+++ src/main/java/com/map/app_map/AppMapApplication.java
@@ -0,0 +1,13 @@
+package com.map.app_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/map/app_map/AppMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.map.app_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
